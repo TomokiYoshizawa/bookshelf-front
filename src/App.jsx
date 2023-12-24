13fcdf387ea98fd1526978e5bbc952d4e553bb9d
tomokinpm <tomoki-goalie@hotmail.com>
2023-12-24 15:45:51 -0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import React from "react";
 import "./App.scss";
 import HomePage from "./pages/HomePage/HomePage";
-import Navigation from "./components/navigation/navigation";
+import Navigation from "./components/Navigation/Navigation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BookCard from "./components/BookCard/BookCard";
 import { SearchProvider } from "./contexts/SearchContext/searchContext";
