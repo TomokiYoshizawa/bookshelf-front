6f360740c502f523ac641c8091ed91107890041c
tomokinpm <tomoki-goalie@hotmail.com>
2023-12-24 15:48:17 -0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import Navigation from "./components/Navigation/Navigation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BookCard from "./components/BookCard/BookCard";
-import { SearchProvider } from "./contexts/SearchContext/searchContext";
+import { SearchProvider } from "./contexts/SearchContext/SearchContext";
 
 function App() {
   return (
